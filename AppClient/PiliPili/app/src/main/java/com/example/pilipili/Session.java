37c17ee9e5122a1d1307c18b15c16d6d3f5db834
gongyue <1024344030@qq.com>
2019-04-24 17:16:02 +0800

    test commit info

--- AppClient/PiliPili/app/src/main/java/com/example/pilipili/Session.java
+++ AppClient/PiliPili/app/src/main/java/com/example/pilipili/Session.java
@@ -2,4 +2,8 @@ package com.example.pilipili;
 
 public class Session {
     public static boolean isLogin = false;
+    public static int user_id = 0;
+    public static void setSession() {
+        isLogin = false;
+    }
 }
