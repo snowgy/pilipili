944648710578dcd36684eff6f36022cd68f0917f
hguandl <hguandl@gmail.com>
2019-05-24 23:32:18 +0800

    Merge With master

--- AppClient/PiliPili/app/src/androidTest/java/com/example/pilipili/LikeTest.java
+++ AppClient/PiliPili/app/src/androidTest/java/com/example/pilipili/LikeTest.java
@@ -34,7 +34,7 @@ import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class FilterTest {
+public class LikeTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
